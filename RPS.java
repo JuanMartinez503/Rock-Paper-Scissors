3cbc1802fdbc2967a117bc488c37c2d0c18e3ab9
Juan Martinez <juanmartinez@Juans-MacBook.local>
2023-09-01 11:37:35 -0400

    I added the scanner feature and it is working as intented

--- RPS.java
+++ RPS.java
@@ -1,7 +1,13 @@
 package rps;
 
+import java.util.Scanner;
+
 public class RPS {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nWelcome to Rock Paper and Scissors!\n");
+        System.out.println("What would you choose? (Please enter the number associated with the choice)\n1. Rock\n2. Paper\n3. Scissors");
+        int choice = scan.nextInt();
+        System.out.println(choice);
     }
 }
