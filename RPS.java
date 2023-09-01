9a29379c42eeb490de23b7a1e8417a00693b5802
Juan Martinez <juanmartinez@Juans-MacBook.local>
2023-09-01 12:17:19 -0400

    I added logic to the case bath based on what the user chooses

--- RPS.java
+++ RPS.java
@@ -6,8 +6,31 @@ public class RPS {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("\nWelcome to Rock Paper and Scissors!\n");
-        System.out.println("What would you choose? (Please enter the number associated with the choice)\n1. Rock\n2. Paper\n3. Scissors");
-        int choice = scan.nextInt();
-        System.out.println(choice);
+        System.out.println("What would you choose? \nRock\nPaper\nScissors");
+        String PlayerChoice = scan.nextLine().toLowerCase();
+        double numberIndex = (int)(Math.random()*3);
+        
+        String[] choicesArray = {"rock","paper","scissors"};
+        String computerChoice = choicesArray[(int) numberIndex];
+        System.out.println(PlayerChoice+computerChoice);
+        scan.close();
+    }
+
+    public static void choosingPath(String choice){
+        switch (choice){
+            case "rock": 
+
+            break;
+            case "paper":
+
+            break;
+            case "scissors":
+
+            break;
+            default:
+            System.out.println("Choice is not a valid answer");
+            break;
+
+        }
     }
 }
