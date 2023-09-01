b505c2dd3750a7ae47c549619c4196b9d95b2725
Juan Martinez <juanmartinez@Juans-MacBook.local>
2023-09-01 12:38:55 -0400

    I erased the case switch statement to add the logic and to handle the error if the user clooses in an invalid choice

--- RPS.java
+++ RPS.java
@@ -7,30 +7,47 @@ public class RPS {
         Scanner scan = new Scanner(System.in);
         System.out.println("\nWelcome to Rock Paper and Scissors!\n");
         System.out.println("What would you choose? \nRock\nPaper\nScissors");
-        String PlayerChoice = scan.nextLine().toLowerCase();
-        double numberIndex = (int)(Math.random()*3);
-        
-        String[] choicesArray = {"rock","paper","scissors"};
+        String playerChoice = scan.nextLine().toLowerCase();
+        double numberIndex = (int) (Math.random() * 3);
+
+        String[] choicesArray = { "rock", "paper", "scissors" };
         String computerChoice = choicesArray[(int) numberIndex];
-        System.out.println(PlayerChoice+computerChoice);
+        System.out.println("Player chose " + playerChoice);
+        System.out.println("Player chose " + computerChoice + "\n");
+        choosingPath(playerChoice, computerChoice);
+
         scan.close();
     }
 
-    public static void choosingPath(String choice){
-        switch (choice){
-            case "rock": 
-
-            break;
+    public static void choosingPath(String choice, String computerChoice) {
+        switch (choice) {
+            case "rock":
+                choosingWinner("rock", computerChoice);
+                break;
             case "paper":
+                choosingWinner("paper", computerChoice);
 
-            break;
+                break;
             case "scissors":
+                choosingWinner("scissors", computerChoice);
 
-            break;
+                break;
             default:
-            System.out.println("Choice is not a valid answer");
-            break;
+                System.out.println("Choice is not a valid answer");
+                break;
 
+        }
+    }
+
+    public static void choosingWinner(String playerChoice, String computerChoice) {
+        if (playerChoice.equals(computerChoice)) {
+            System.out.println("It's a tie!");
+        } else if ((playerChoice.equals("rock") && computerChoice.equals("scissors")) ||
+                (playerChoice.equals("scissors") && computerChoice.equals("paper")) ||
+                (playerChoice.equals("paper") && computerChoice.equals("rock"))) {
+            System.out.println("You win!");
+        } else {
+            System.out.println("Computer wins!");
         }
     }
 }
